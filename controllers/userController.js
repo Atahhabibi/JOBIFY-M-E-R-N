d3bd5cd34fe0f9d4555a1726df79621128c3d400
atahhabibi <habibiatah@gmail.com>
2023-08-16 09:04:54 -0700

    fixed update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,49 +1,41 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../modals/userModel.js";
 import Job from "../modals/jobModels.js";
-import cloudinary from 'cloudinary'
-import { promises as fs } from "fs";
-
-
+import cloudinary from "cloudinary";
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
+  const user = await User.findOne({ _id: req.user.userId });
 
-  const user=await User.findOne({_id:req.user.userId});
+  const userWithoutPassword = user.toJSON();
 
-  const userWithoutPassword=user.toJSON();
-
-  res.status(StatusCodes.OK).json({ user:userWithoutPassword });
+  res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
 
 export const getApplicationStats = async (req, res) => {
-
-  const users=await User.countDocuments();
-  const jobs=await Job.countDocuments();
-  res.status(StatusCodes.OK).json({ users,jobs});
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
+  res.status(StatusCodes.OK).json({ users, jobs });
 };
 
+export const updateUser = async (req, res) => {
+  const newUser = { ...req.body };
+  delete newUser.password;
+  console.log(req.file);
 
+  if (req.file) {
+    const file = formatImage(req.file);
+    const response = await cloudinary.v2.uploader.upload(file);
 
-export const updateUser=async(req,res)=>{
-  
-    const newUser={...req.body};
-    delete newUser.password;
-    console.log(req.file);
-
-   if(req.file){
-     const response=await cloudinary.v2.uploader.upload(req.file.path);
-     await fs.unlink(req.file.path);
-     newUser.avatar=response.secure_url;
-     newUser.avatarPublicId=response.public_id;
-   }
-
-  const udpatedUser=await User.findByIdAndUpdate(req.user.userId,newUser);
-
-  if(req.file && udpatedUser.avatarPublicId){
-    await cloudinary.v2.uploader.destroy(udpatedUser.avatarPublicId)
-
+    newUser.avatar = response.secure_url;
+    newUser.avatarPublicId = response.public_id;
   }
 
-  res.status(StatusCodes.OK).json({msg:'user updated'})
+  const udpatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
 
-}
+  if (req.file && udpatedUser.avatarPublicId) {
+    await cloudinary.v2.uploader.destroy(udpatedUser.avatarPublicId);
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "user updated" });
+};
